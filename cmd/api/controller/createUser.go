fdffb68b1cdc51562e84d8c735e099a6f62c92bc
Morpa <andremorpa@gmail.com>
2023-12-29 16:03:29 +0000

    add create user validation

--- cmd/api/controller/createUser.go
+++ cmd/api/controller/createUser.go
@@ -1,5 +1,22 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/Morpa/go-crud/cmd/api/configuration/validation"
+	"github.com/Morpa/go-crud/cmd/api/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
+		return
+	}
+
+	fmt.Println(userRequest)
+}
