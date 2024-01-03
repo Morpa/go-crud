e5bc55ad1a77907073177f2f364d67e444c72dd6
Morpa <andremorpa@gmail.com>
2024-01-03 19:39:41 +0000

    add JWT middleware

--- src/api/controller/routes/routes.go
+++ src/api/controller/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/Morpa/go-crud/src/api/controller"
+	"github.com/Morpa/go-crud/src/api/model"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,8 +10,8 @@ func InitRoutes(
 	r *gin.RouterGroup,
 	userController controller.UserControllerInterface,
 ) {
-	r.GET("/getUserById/:userId", userController.FindUserByID)
-	r.GET("/getUserByEmail/:userEmail", userController.FindUserByEmail)
+	r.GET("/getUserById/:userId", model.VerifyTokenMiddleware, userController.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", model.VerifyTokenMiddleware, userController.FindUserByEmail)
 	r.POST("/createUser", userController.CreateUser)
 	r.PUT("/updateUser/:userId", userController.UpdateUser)
 	r.DELETE("/deleteUser/:userId", userController.DeleteUser)
